b722d919475de055112449c412632fd5372e91a9
Brandon Parris <brandonparrisguy@gmail.com>
2019-07-08 16:59:22 +0000

    Automatically backed up by Learn

---
+++ lib/intro_to_simple_array_manipulations.rb
@@ -0,0 +1,6 @@
+describe "#using_push" do
+def using_push = [colors_in_the_rainbow, next_color];
+colors_in_the_rainbow = ("red", "orange", "yellow", "green", "blue", "indigo");
+next_color = ("violet");
+return using_push.push (next_color)
+end
