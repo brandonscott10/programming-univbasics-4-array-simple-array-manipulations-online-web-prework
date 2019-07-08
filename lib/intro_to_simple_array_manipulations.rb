76fa8e27f81a1fb6e04eb478a56eb01b0e32c4fe
Brandon Parris <brandonparrisguy@gmail.com>
2019-07-08 17:29:44 +0000

    Automatically backed up by Learn

--- lib/intro_to_simple_array_manipulations.rb
+++ lib/intro_to_simple_array_manipulations.rb
@@ -2,3 +2,7 @@ def using_push(colors_in_the_rainbow, next_color)
 
 return colors_in_the_rainbow.push(next_color)
 end
+def using_push(colors_in_the_rainbow, next_color)
+
+return colors_in_the_rainbow.unshift(next_color)
+end
